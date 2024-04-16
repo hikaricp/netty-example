ed6da91446d0f017979453745cd3e351ba860d01
Jiachang Yang <dockerc@163.com>
2024-04-16 22:35:58 +0800

    添加 HttpServer 处理器

--- src/main/java/com/example/primary/HttpServer.java
+++ src/main/java/com/example/primary/HttpServer.java
@@ -1,5 +1,8 @@
 package com.example.primary;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.EventLoopGroup;
@@ -7,17 +10,25 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 
 public class HttpServer {
+
+    private static final Logger logger = LoggerFactory.getLogger(HttpServer.class);
+
+    private static final int PORT = Integer.parseInt(System.getProperty("port", "8888"));
+
     public static void main(String[] args) throws InterruptedException {
         EventLoopGroup parentGroup = new NioEventLoopGroup();
         NioEventLoopGroup childGroup = new NioEventLoopGroup();
-        
+
         try {
             ServerBootstrap bootstrap = new ServerBootstrap();
             bootstrap.group(parentGroup, childGroup)
                     .channel(NioServerSocketChannel.class)
                     .childHandler(new HttpChannelInitializer());
 
-            ChannelFuture future = bootstrap.bind(8888).sync();
+            ChannelFuture future = bootstrap.bind(PORT).sync();
+
+            logger.info("http server start, port: {}", PORT);
+
             future.channel().closeFuture().sync();
         } finally {
             parentGroup.shutdownGracefully();
---
+++ src/main/java/com/example/primary/HttpServerHandler.java
@@ -0,0 +1,52 @@
+package com.example.primary;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpVersion;
+import io.netty.util.CharsetUtil;
+
+public class HttpServerHandler extends ChannelInboundHandlerAdapter {
+
+    private static final Logger logger = LoggerFactory.getLogger(HttpServerHandler.class);
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        if (msg instanceof HttpRequest) {
+            HttpRequest request = (HttpRequest) msg;
+            logger.info("请求方式 => {}", request.method().name());
+            logger.info("请求 URI => {}", request.uri());
+
+            if ("/favico.icon".equals(request.uri())) {
+                logger.info("不处理/favico.icon请求");
+                return;
+            }
+
+            ByteBuf content = Unpooled.copiedBuffer("Hello, world!", CharsetUtil.UTF_8);
+            DefaultFullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK,
+                    content);
+            HttpHeaders headers = response.headers();
+            headers.set(HttpHeaderNames.CONTENT_TYPE, "text/plain");
+            headers.set(HttpHeaderNames.CONTENT_LENGTH, content.readableBytes());
+
+            ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+}
